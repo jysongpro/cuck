eba9f839a3d6375d71751c09c69aeeb6c4bef12b
jysongpro <jysongpro@gmail.com>
2026-09-25 15:24:36 +0900

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -12,14 +12,14 @@
  *    표시되는 firebaseConfig 값을 아래에 붙여넣습니다.
  * ========================================================================= */
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+    apiKey: "AIzaSyCZI83_8mOYcBJoFvcb6D4MV5DNOBGJjIQ",
+    authDomain: "cuck-3907f.firebaseapp.com",
+    projectId: "cuck-3907f",
+    storageBucket: "cuck-3907f.firebasestorage.app",
+    messagingSenderId: "991339973082",
+    appId: "1:991339973082:web:aac5db7950efd3498418e3",
+    measurementId: "G-QHK8HJPQBG" 
 };
-
 /* 여러 학과/캠퍼스가 데이터를 분리해서 쓰고 싶다면 URL에 ?ws=학과코드 를 붙이면
  * 서로 다른 문서에 저장됩니다. 지정하지 않으면 'default' 공유 문서를 사용합니다. */
 window.FIREBASE_WORKSPACE =
